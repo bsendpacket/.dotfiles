7b6c8414ec5ce5d9dd26cc0ec0e5d57e1f47c282
0x1c <kbalint@0x1c.zip>
2024-03-17 22:53:10 -0400

    Add Hexyl to home-manager

--- home-manager/yazi/yazi.nix
+++ home-manager/yazi/yazi.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
+
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
@@ -19,114 +20,139 @@
         cwd = { fg = "#85dc85"; };
         hovered = { reversed = true; };
         preview_hovered = { underline = true; };
+
         find_keyword = {
           fg = "#c6c684";
           bold = true;
           italic = true;
           underline = true;
         };
+
         find_position = { 
           fg = "#ae81ff";
           bg = "reset";
           bold = true;
           italic = true;
         };
+
         marker_copied = {
           fg = "#36c692";
           bg = "#36c692";
         };
+
         marker_cut = {
           fg = "#ff5189";
           bg = "#ff5189";
         };
+
         marker_marked = {
           fg = "#c6c684";
           bg = "#c6c684";
         };
+
         marker_selected = {
           fg = "#74b2ff";
           bg = "#74b2ff";
         };
+
         tab_active = {
           fg = "#080808";
           bg = "#e4e4e4";
         };
+
         tab_inactive = {
           fg = "#e4e4e4";
           bg = "#323437";
         };
+
         tab_width = 1;
         count_copied = {
           fg = "#080808";
           bg = "#36c692";
         };
+
         count_cut = {
           fg = "#080808";
           bg = "#ff5189";
         };
+
         count_selected = {
           fg = "#080808";
           bg = "#74b2ff";
         };
+
         border_symbol = "│";
         border_style = { fg = "#949494"; };
       };
+      
       status = {
         separator_open = "";
         separator_close = "";
+
         separator_style = {
           fg = "#323437";
           bg = "#323437";
         };
+
         mode_normal = {
           fg = "#080808";
           bg = "#74b2ff";
           bold = true;
         };
+
         mode_select = {
           fg = "#080808";
           bg = "#36c692";
           bold = true;
         };
+
         mode_unset = {
           fg = "#080808";
           bg = "#ff5189";
           bold = true;
         };
+
         progress_label = {
           fg = "#e4e4e4";
           bold = true;
         };
+
         progress_normal = {
           fg = "#74b2ff";
           bg = "#323437";
         };
+
         progress_error = {
           fg = "#ff5189";
           bg = "#323437";
         };
+
         permissions_t = { fg = "#74b2ff"; };
         permissions_r = { fg = "#c6c684"; };
         permissions_w = { fg = "#ff5189"; };
         permissions_x = { fg = "#36c692"; };
         permissions_s = { fg = "#949494"; };
       };
+
       input = {
         border = { fg = "#74b2ff"; };
         title = {};
         value = {};
         selected = { reversed = true; };
       };
+
       select = {
         border = { fg = "#74b2ff"; };
         active = { fg = "#ae81ff"; };
         inactive = {};
       };
+
       tasks = {
         border = { fg = "#74b2ff"; };
         title = {};
         hovered = { underline = true; };
       };
+
       which = {
         mask = { bg = "#323437"; };
         cand = { fg = "#85dc85"; };
@@ -135,6 +161,7 @@
         separator = "  ";
         separator_style = { fg = "#949494"; };
       };
+
       help = {
         on = { fg = "#ae81ff"; };
         exec = { fg = "#85dc85"; };
@@ -148,6 +175,7 @@
           bg = "#e4e4e4";
         };
       };
+
       filetype = {
         rules = [
           { mime = "image/*"; fg = "#85dc85"; }
